4c76414d0f206e1aa8c11e104180afdf11563afe
fatimazahramassane <fatimazahra.massane-etu@etu.univh2c.ma>
2026-03-01 00:43:49 +0000

    commit

--- src/main/java/net/Massane/pres/Pres2.java
+++ src/main/java/net/Massane/pres/Pres2.java
@@ -7,11 +7,11 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Pres2 {
-    public static void main(String[] args)throws FileNotFoundException , ClassNotFoundException, InstantiationException , IllegalAccessException, NoSuchMethodException{
+    public static void main(String[] args)throws Exception{
             Scanner scanner = new Scanner(new File("config.txt"));
             String daoClassName = scanner.nextLine();
             Class cDao = Class.forName(daoClassName);
-            IDao dao =(IDao) cDao.newInstance();
+            IDao d =(IDao) cDao.newInstance();
 
             String metierClassName = scanner.nextLine();
             Class cMetier = Class.forName(metierClassName);
